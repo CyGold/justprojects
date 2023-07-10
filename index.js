3c343313cd2443cf7c57817d84a3ac01d7011099
Celyn Yusuf <goldeous4@gmail.com>
2023-07-10 21:06:28 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const hamburger = document.querySelector('.hamburger')
-const nav = document.querySelector('.link')
+const nav2 = document.querySelector('.nav2')
+const close = document.querySelector('.closemenu')
 
 hamburger.addEventListener('click', ()=> {
-    nav.classList.toggle('visible')
+    nav2.classList.toggle('visible');
+    close.classList.remove('hide');
+    hamburger.classList.add('hide')
 })
+
+close.addEventListener ('click', ()=> {
+   close.classList.add('hide');
+   hamburger.classList.remove('hide');
+   nav2.classList.toggle('visible');
+})
+    
+
